d214cfa8be45f1522fc0c81b18d50fa5a6a0b6fd
Juan Pablo Pagotto <jppcoder@gmail.com>
2021-03-27 08:49:24 -0300

    cambios previos a nueva rama

---
+++ js/datos.js
@@ -0,0 +1,35 @@
+const DATOS = [{
+    "id": 1,
+    "nombre": "Neurociencias",
+    "descripcion": "Curso de: Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero et deleniti consectetur incidunt iste voluptates? Esse voluptatem, corrupti quam praesentium molestiae veritatis quod deserunt minima eum modi quidem amet!",
+    "price": 500,
+    "img": "../img/neuro.png"
+  }, {
+    "id": 2,
+    "nombre": "ProgramacionPNL",
+    "descripcion": "Curso de: Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero et deleniti consectetur incidunt iste voluptates? Esse voluptatem, corrupti quam praesentium molestiae veritatis quod deserunt minima eum modi quidem amet!",
+    "price": 900,
+    "img":"../img/pnl.jpg"
+  }, {
+    "id": 3,
+    "nombre": "Coaching",
+    "descripcion": "Curso de: Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero et deleniti consectetur incidunt iste voluptates? Esse voluptatem, corrupti quam praesentium molestiae veritatis quod deserunt minima eum modi quidem amet!",
+    "price": 1000,
+    "img": "../img/coach.jpg"
+  },{
+    "id": 4,
+    "nombre": "Biodescodificacion",
+    "descripcion": "Curso de: Snsectetur a et deleniti consectetur incidunt iste voluptates? Esse voluptatem, corrupti quam praesentium molestiae veritatis quod deserunt minima eum modi quidem amet!",
+    "price": 1800,
+    "img": "../img/bio.jpg"
+  },{
+    "id": 5,
+    "nombre": "Coaching",
+    "descripcion": "Curso de: Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero et deleniti consectetur incidunt iste voluptates? Esse voluptatem, corrupti quam praesentium molestiae veritatis quod deserunt minima eum modi quidem amet!",
+    "price": 1500,
+    "img": "../img/coach.jpg"
+  }
+
+
+];
+
